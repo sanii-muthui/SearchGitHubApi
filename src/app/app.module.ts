31ef338abec8a9b5907c8ef7ec7e89048aa6ddab
sanii-muthui <muthuisanii@gmail.com>
2019-07-15 17:31:39 +0300

    ngModule and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
 import { AppComponent } from './app.component';
-
+import {HttpClientModule} from "@angular/common/http"
+import { SearchService } from './search.service';
+import { UserComponent } from './user/user.component';
+import { RepoDetailsComponent } from './repo-details/repo-details.component';
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
+import { DatePipe } from './date.pipe';
+import { LandComponent } from './land/land.component';
+import {RoutingModule} from "./routing/routing.module";
+import { BoldsDirective } from './bolds.directive'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    RepoDetailsComponent,
+    DatePipe,
+    LandComponent,
+    BoldsDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    NgProgressModule,
+    NgProgressHttpModule,
+    LoadingBarHttpClientModule,
+    RoutingModule
   ],
-  providers: [],
+  providers: [SearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
