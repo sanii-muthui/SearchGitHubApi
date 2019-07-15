7fabb9928dd29dde1f9c3bd467cab34220c7cd8f
sanii-muthui <muthuisanii@gmail.com>
2019-07-15 17:32:47 +0300

    directives file

---
+++ src/app/bolds.directive.spec.ts
@@ -0,0 +1,8 @@
+import { BoldsDirective } from './bolds.directive';
+
+describe('BoldsDirective', () => {
+  it('should create an instance', () => {
+    const directive = new BoldsDirective();
+    expect(directive).toBeTruthy();
+  });
+});
